39fc05a0a212bc31412e2785d40fdf785f88be00
zaytsev <zaytsev@slotlogic.eu>
2025-04-15 14:59:55 +0200

    add a function for open login link, lesson 4.1.7

--- test_main_page.py
+++ test_main_page.py
@@ -3,7 +3,11 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_guest_should_see_login_link(browser):
-    browser.get(link)
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    go_to_login_page(browser)
