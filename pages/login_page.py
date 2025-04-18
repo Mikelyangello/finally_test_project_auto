9e63d086de67c0d7be8541bf6b4aa00fc340374e
zaytsev <zaytsev@slotlogic.eu>
2025-04-18 19:45:26 +0200

    add a login_page dfile

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "Sorry, we don't see a 'login' string in url"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), f"Can't find locator '{LoginPageLocators.LOGIN_FORM[1]}'"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), f"Can't find locator '{LoginPageLocators.REGISTER_FORM[1]}'"
