e23c67162ca6faa3752176d41d791ca4168609e8
zaytsev <zaytsev@slotlogic.eu>
2025-05-05 17:55:59 +0200

    End of lesson 4.3.4. Add a parametrizer and function of create links

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,15 @@
 from .pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+def link_compilator(excluded_link=None, number_of_links=10):
+    base = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+    return [base + str(i) if i != excluded_link else pytest.param(base + str(i), marks=pytest.mark.xfail)
+            for i in range(number_of_links)]
+
+
+@pytest.mark.parametrize('link', link_compilator(7))
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
