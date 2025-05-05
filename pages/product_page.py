1b8a286546be7b55feace596e144c195cffe992a
zaytsev <zaytsev@slotlogic.eu>
2025-05-05 19:30:43 +0200

    End of lesson 4.3.5. Add a special waiting methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -38,4 +38,14 @@ class ProductPage(BasePage):
             f"PRICES of products IS NOT similar\n\
                 from card: {product_price_from_card} != {product_price_from_notification} - from notification"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message still is presented, but should not be"
+
+
+
 
